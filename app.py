c566db5d4845f2882241b8a7d8a7431328df4c9f
NGUYEN LUC <96407@dmt.local>
2026-04-08 00:07:42 +0900

    update code

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host='0.0.0.0', port=port)
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
